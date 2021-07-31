da56ae686a9aef70e6fb3ebb058ff1c6d8939494
Ankit Kumar <ankitdayal1992@gmail.com>
2021-07-31 19:24:04 +0530

    components restructured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { SidebarComponent } from './pages/admin/sidebar/sidebar.component';
+import { WelcomeComponent } from './pages/admin/welcome/welcome.component';
+import { ComingSoonComponent } from './pages/coming-soon/coming-soon.component';
+import { GenresComponent } from './pages/admin/genres/genres.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/admin/profile/profile.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SubjectsComponent } from './pages/admin/subjects/subjects.component';
+import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    DashboardComponent,
+    SidebarComponent,
+    WelcomeComponent,
+    ComingSoonComponent,
+    GenresComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    SignupComponent,
+    SubjectsComponent,
+    UserDashboardComponent
   ],
   imports: [
     BrowserModule,
