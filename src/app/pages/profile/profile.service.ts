bb97d3b39c35ecf1232f3f2c1f951872def5308b
Ankit Kumar <ankitdayal1992@gmail.com>
2021-10-09 17:11:14 +0530

    Comin Soon Component image path updated

--- src/app/pages/profile/profile.service.ts
+++ src/app/pages/profile/profile.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,11 +8,25 @@ import { Observable } from 'rxjs';
 export class ProfileService {
 
   APP_URL = 'https://i-exam.herokuapp.com';
+
+  
   
   constructor(private http: HttpClient) { }
 
   getProfile(userId: number): Observable<any> {
+    const headerDict = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods' : 'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    }
+    
+    const requestOptions = {                                                                                                                                                                                 
+      headers: new HttpHeaders(headerDict), 
+    };
+
     let url = `${this.APP_URL}/test`;
-    return this.http.get(url);
+    return this.http.get(url, requestOptions);
   }
 }
