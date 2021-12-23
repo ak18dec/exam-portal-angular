5078cefbc303a0fdca92f50aa77278b0c86b0c3a
Ankit Kumar <ankitdayal1992@gmail.com>
2021-12-24 01:38:01 +0530

    New User Component Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { ProfileComponent } from './modules/profile/profile.component';
 import { HomeComponent } from './layouts/home/home.component';
 import { UsersComponent } from './modules/users/users.component';
+import { UserComponent } from './modules/users/user/user.component';
 
 const routes: Routes = [
   {
@@ -47,6 +48,11 @@ const routes: Routes = [
       {
         path: 'users',
         component: UsersComponent
+      },
+      {
+        path: 'users/new',
+        component: UserComponent,
+        pathMatch: 'full'
       },
       {
         path: 'genres',
--- src/app/layouts/defaults/defaults.module.ts
+++ src/app/layouts/defaults/defaults.module.ts
@@ -14,6 +14,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { ProfileComponent } from 'src/app/modules/profile/profile.component';
 import { RouterModule } from '@angular/router';
 import { UsersComponent } from 'src/app/modules/users/users.component';
+import { UserComponent } from 'src/app/modules/users/user/user.component';
 
 
 
@@ -29,7 +30,8 @@ import { UsersComponent } from 'src/app/modules/users/users.component';
     SubjectsComponent,
     GenresComponent,
     ProfileComponent,
-    UsersComponent
+    UsersComponent,
+    UserComponent
   ],
   imports: [
     CommonModule,
