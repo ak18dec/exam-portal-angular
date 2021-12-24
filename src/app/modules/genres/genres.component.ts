8c34c1bb85b30a8cb822a0c9de1f8e78d5bb2fc2
Ankit Kumar <ankitdayal1992@gmail.com>
2021-12-25 01:02:43 +0530

    Genres and Subjects Components Updated v2

--- src/app/modules/genres/genres.component.ts
+++ src/app/modules/genres/genres.component.ts
@@ -5,6 +5,10 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { GenreService } from 'src/app/services/genre.service';
 import { Genre } from 'src/app/models/genre';
 
+
+import { MatSort } from '@angular/material/sort';
+
+
 @Component({
   selector: 'app-genres',
   templateUrl: './genres.component.html',
@@ -12,178 +16,223 @@ import { Genre } from 'src/app/models/genre';
 })
 export class GenresComponent implements OnInit {
 
-  newGenre: boolean = false;
+  // newGenre: boolean = false;
 
-  addBtnClicked: boolean = false;
+  // addBtnClicked: boolean = false;
 
-  genres: Genre[] = [];
+  // genres: Genre[] = [];
 
-  // displayedColumns: string[] = ['position', 'title', 'weight', 'status'];
-  // dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  // @ViewChild(MatPaginator) paginator: MatPaginator;
+
 
   
 
-  newGenreData: Genre = {
-    id: 0,
-    title: '',
-    description: '',
-    enabled: true
-  }
+  // newGenreData: Genre = {
+  //   id: 0,
+  //   title: '',
+  //   description: '',
+  //   enabled: true
+  // }
 
-  constructor(private _snackBar: MatSnackBar, private genreService: GenreService) { }
+  // constructor(private _snackBar: MatSnackBar, private genreService: GenreService) { }
 
-  ngOnInit() {
-    this.freshForm();
-    this.getAllGenres();
-    // this.dataSource.paginator = this.paginator;
-  }
+  // ngOnInit() {
+  //   this.freshForm();
+  //   this.getAllGenres();
+   
+  // }
 
-  getAllGenres(){
-    this.genreService.getGenres().subscribe(
-      (res: any)=>{
-        this.genres = res;
-      },
-      (error)=>{
-        this._snackBar.open('Error while fetching genres list','',{
-          duration: 3000
-        });
-      }
-    )
-  }
+  // getAllGenres(){
+  //   this.genreService.getGenres().subscribe(
+  //     (res: any)=>{
+  //       this.genres = res;
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open('Error while fetching genres list','',{
+  //         duration: 3000
+  //       });
+  //     }
+  //   )
+  // }
 
-  addGenre(){
-    this.freshForm();
-    this.newGenre = true;
-    this.addBtnClicked = true;
-  }
+  // addGenre(){
+  //   this.freshForm();
+  //   this.newGenre = true;
+  //   this.addBtnClicked = true;
+  // }
 
-  submitForm(newGenreForm: any){
+  // submitForm(newGenreForm: any){
 
-    if(!this.newGenreData.title){
-      this._snackBar.open('Please provide genre title','',{
-        duration: 3000
-      });
-      return;
-    }
+  //   if(!this.newGenreData.title){
+  //     this._snackBar.open('Please provide genre title','',{
+  //       duration: 3000
+  //     });
+  //     return;
+  //   }
 
-    if(this.addBtnClicked){
-      this.createNewGenre();
-    }else{
-      this.updateGenre();
-    }
+  //   if(this.addBtnClicked){
+  //     this.createNewGenre();
+  //   }else{
+  //     this.updateGenre();
+  //   }
 
     
 
-  }
+  // }
 
-  editGenre(editableGenre: Genre){
-    this.newGenre = true;
-    this.addBtnClicked = false;
-    this.newGenreData.title = editableGenre.title;
-    this.newGenreData.description = editableGenre.description;
-    this.newGenreData.id = editableGenre.id;
-    this.newGenreData.enabled = editableGenre.enabled;
-  }
+  // editGenre(editableGenre: Genre){
+  //   this.newGenre = true;
+  //   this.addBtnClicked = false;
+  //   this.newGenreData.title = editableGenre.title;
+  //   this.newGenreData.description = editableGenre.description;
+  //   this.newGenreData.id = editableGenre.id;
+  //   this.newGenreData.enabled = editableGenre.enabled;
+  // }
 
-  createNewGenre(){
-    this.genreService.addGenre(this.newGenreData).subscribe(
-      (data: any)=>{
-        this.genres.push(data);
-        this._snackBar.open(`${this.newGenreData.title} is added successfully to list`,'',{
-          duration: 3000
-        });
+  // createNewGenre(){
+  //   this.genreService.addGenre(this.newGenreData).subscribe(
+  //     (data: any)=>{
+  //       this.genres.push(data);
+  //       this._snackBar.open(`${this.newGenreData.title} is added successfully to list`,'',{
+  //         duration: 3000
+  //       });
 
-        this.freshForm();
+  //       this.freshForm();
         
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open(error,'',{
+  //         duration: 3000
+  //       });
+  //     },
+  //     () =>{
+  //       this.addBtnClicked = false;
+  //     }
+  //   );
+  // }
+
+  // updateGenre(){
+  //   this.genreService.updateGenre(this.newGenreData, this.newGenreData.id).subscribe(
+  //     (data)=>{
+  //       if (data) {
+  //         let idxToUpdate = this.genres.findIndex(g=>g.id === this.newGenreData.id);
+  //         this.genres[idxToUpdate].title = this.newGenreData.title;
+  //         this.genres[idxToUpdate].description = this.newGenreData.description;
+
+  //         this._snackBar.open('Genre updated successfully', '', {
+  //           duration: 3000
+  //         });
+  //         this.freshForm();
+  //       }
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open(error,'',{
+  //         duration: 3000
+  //       });
+  //     }
+  //   );
+  // }
+
+  // backToGenres(){
+  //   this.freshForm();
+  // }
+
+  // toggleGenreStatus(id: number, idx: number){
+
+  //   let oldState = this.genres[idx].enabled;
+  //   this.genres[idx].enabled = !oldState;
+
+  //   this.genreService.toggleGenreState(this.genres[idx], id).subscribe(
+  //     (res)=>{
+  //       if (res) {
+  //         let status = this.genres[idx].enabled ? 'enabled' : 'disabled';
+  //         this._snackBar.open(`Genre ${status} successfully`, '', {
+  //           duration: 3000
+  //         });
+  //       }
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open(error,'',{
+  //         duration: 3000
+  //       });
+  //     }
+  //   )
+  // }
+
+  // freshForm(){
+  //   this.newGenre = false;
+  //   this.addBtnClicked = false;
+  //   this.newGenreData.id = 0
+  //   this.newGenreData.title = '';
+  //   this.newGenreData.description = '';
+  //   this.newGenreData.enabled = true;
+  // }
+
+  // deleteGenre(id: number){
+  //   this.genreService.deleteGenre(id).subscribe(
+  //     (res)=>{
+  //       if (res) {
+  //         let idxToDelete = this.genres.findIndex(g => g.id === id);
+  //         this.genres.splice(idxToDelete, 1);
+
+  //         this._snackBar.open(`Genre removed successfully`, '', {
+  //           duration: 3000
+  //         });
+  //         this.freshForm();
+  //       }
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open(error,'',{
+  //         duration: 3000
+  //       });
+  //     }
+  //   );
+  // }
+
+
+  dataSource: MatTableDataSource<any>;
+
+  genres: Genre[]=[];
+
+  columns: string[] = ['id','title', 'description', 'enabled']
+
+  @ViewChild(MatSort, { static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true}) paginator: MatPaginator;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.genres = [
+      {
+        id: 1,
+        title: 'Academics',
+        description: 'Dummy Text',
+        enabled: true
       },
-      (error)=>{
-        this._snackBar.open(error,'',{
-          duration: 3000
-        });
+      {
+        id: 2,
+        title: 'Arts & Culture',
+        description: 'Dummy Text',
+        enabled: true
       },
-      () =>{
-        this.addBtnClicked = false;
+      {
+        id: 3,
+        title: 'Entertainment',
+        description: 'Dummy Text',
+        enabled: true
       }
-    );
+    ]
+   
+    this.dataSource = new MatTableDataSource(this.genres);
+
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
-  updateGenre(){
-    this.genreService.updateGenre(this.newGenreData, this.newGenreData.id).subscribe(
-      (data)=>{
-        if (data) {
-          let idxToUpdate = this.genres.findIndex(g=>g.id === this.newGenreData.id);
-          this.genres[idxToUpdate].title = this.newGenreData.title;
-          this.genres[idxToUpdate].description = this.newGenreData.description;
-
-          this._snackBar.open('Genre updated successfully', '', {
-            duration: 3000
-          });
-          this.freshForm();
-        }
-      },
-      (error)=>{
-        this._snackBar.open(error,'',{
-          duration: 3000
-        });
-      }
-    );
+  applyFilter(event: any){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  backToGenres(){
-    this.freshForm();
-  }
-
-  toggleGenreStatus(id: number, idx: number){
-
-    let oldState = this.genres[idx].enabled;
-    this.genres[idx].enabled = !oldState;
-
-    this.genreService.toggleGenreState(this.genres[idx], id).subscribe(
-      (res)=>{
-        if (res) {
-          let status = this.genres[idx].enabled ? 'enabled' : 'disabled';
-          this._snackBar.open(`Genre ${status} successfully`, '', {
-            duration: 3000
-          });
-        }
-      },
-      (error)=>{
-        this._snackBar.open(error,'',{
-          duration: 3000
-        });
-      }
-    )
-  }
-
-  freshForm(){
-    this.newGenre = false;
-    this.addBtnClicked = false;
-    this.newGenreData.id = 0
-    this.newGenreData.title = '';
-    this.newGenreData.description = '';
-    this.newGenreData.enabled = true;
-  }
-
-  deleteGenre(id: number){
-    this.genreService.deleteGenre(id).subscribe(
-      (res)=>{
-        if (res) {
-          let idxToDelete = this.genres.findIndex(g => g.id === id);
-          this.genres.splice(idxToDelete, 1);
-
-          this._snackBar.open(`Genre removed successfully`, '', {
-            duration: 3000
-          });
-          this.freshForm();
-        }
-      },
-      (error)=>{
-        this._snackBar.open(error,'',{
-          duration: 3000
-        });
-      }
-    );
-  }
 
 }
--- src/app/modules/subjects/subjects.component.ts
+++ src/app/modules/subjects/subjects.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Subject } from 'src/app/models/subject';
 import { GenreService } from 'src/app/services/genre.service';
 import { SubjectService } from 'src/app/services/subject.service';
+
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-subjects',
@@ -11,191 +15,241 @@ import { SubjectService } from 'src/app/services/subject.service';
 })
 export class SubjectsComponent implements OnInit {
 
-  newSubject: boolean = false;
-  addBtnClicked: boolean = false;
-  subjects: Subject[] = [];
-  genresList: any[] = [];
+  // newSubject: boolean = false;
+  // addBtnClicked: boolean = false;
+  // subjects: Subject[] = [];
+  // genresList: any[] = [];
 
-  newSubjectData: Subject = {
-    id: 0,
-    title: '',
-    description: '',
-    genreId: 0,
-    enabled: true
-  }
+  // newSubjectData: Subject = {
+  //   id: 0,
+  //   title: '',
+  //   description: '',
+  //   genreId: 0,
+  //   enabled: true
+  // }
 
-  constructor(private _snackBar: MatSnackBar, private subjectService: SubjectService, private genreService: GenreService) { }
+  // constructor(private _snackBar: MatSnackBar, private subjectService: SubjectService, private genreService: GenreService) { }
 
-  ngOnInit() {
-    this.freshForm();
-    this.getAllSubjects();
-    this.genresList.length = 0;
-    this.genreService.getGenres().subscribe(
-      (data: any)=>{
-        data.forEach((d: { id: any; title: any; enabled: any; })=> {
-          if(d.enabled){
-            this.genresList.push({id: d.id, label: d.title, enabled: d.enabled});
-          }
-        });
-      },
-      (error)=>{
-        this._snackBar.open('Error while fetching genres list','',{
-          duration: 3000
-        });
-      }
-    );
-  }
+  // ngOnInit() {
+  //   this.freshForm();
+  //   this.getAllSubjects();
+  //   this.genresList.length = 0;
+  //   this.genreService.getGenres().subscribe(
+  //     (data: any)=>{
+  //       data.forEach((d: { id: any; title: any; enabled: any; })=> {
+  //         if(d.enabled){
+  //           this.genresList.push({id: d.id, label: d.title, enabled: d.enabled});
+  //         }
+  //       });
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open('Error while fetching genres list','',{
+  //         duration: 3000
+  //       });
+  //     }
+  //   );
+  // }
 
-  getAllSubjects() {
-    this.subjectService.getSubjects().subscribe(
-      (res: any) => {
-        this.subjects = res;
-      },
-      (error) => {
-        this._snackBar.open('Error while fetching subjects list','',{
-          duration: 3000
-        });
-      } 
-    )
-  }
+  // getAllSubjects() {
+  //   this.subjectService.getSubjects().subscribe(
+  //     (res: any) => {
+  //       this.subjects = res;
+  //     },
+  //     (error) => {
+  //       this._snackBar.open('Error while fetching subjects list','',{
+  //         duration: 3000
+  //       });
+  //     } 
+  //   )
+  // }
 
-  freshForm(){
-    this.newSubject = false;
-    this.addBtnClicked = false;
-    this.newSubjectData.id = 0
-    this.newSubjectData.title = '';
-    this.newSubjectData.description = '';
-    this.newSubjectData.genreId = 0;
-    this.newSubjectData.enabled = true;
-  }
+  // freshForm(){
+  //   this.newSubject = false;
+  //   this.addBtnClicked = false;
+  //   this.newSubjectData.id = 0
+  //   this.newSubjectData.title = '';
+  //   this.newSubjectData.description = '';
+  //   this.newSubjectData.genreId = 0;
+  //   this.newSubjectData.enabled = true;
+  // }
 
-  addSubject() {
-    this.freshForm();
-    this.newSubject = true;
-    this.addBtnClicked = true;
-  }
+  // addSubject() {
+  //   this.freshForm();
+  //   this.newSubject = true;
+  //   this.addBtnClicked = true;
+  // }
 
-  backToSubjects(){
-    this.freshForm();
-  }
+  // backToSubjects(){
+  //   this.freshForm();
+  // }
 
-  editSubject(editableSubject: Subject){
-    this.newSubject = true;
-    this.addBtnClicked = false;
-    this.newSubjectData.title = editableSubject.title;
-    this.newSubjectData.description = editableSubject.description;
-    this.newSubjectData.id = editableSubject.id;
-    this.newSubjectData.enabled = editableSubject.enabled;
-    this.newSubjectData.genreId = editableSubject.genreId;
-  }
+  // editSubject(editableSubject: Subject){
+  //   this.newSubject = true;
+  //   this.addBtnClicked = false;
+  //   this.newSubjectData.title = editableSubject.title;
+  //   this.newSubjectData.description = editableSubject.description;
+  //   this.newSubjectData.id = editableSubject.id;
+  //   this.newSubjectData.enabled = editableSubject.enabled;
+  //   this.newSubjectData.genreId = editableSubject.genreId;
+  // }
 
-  deleteSubject(id: number){
-    this.subjectService.deleteSubject(id).subscribe(
-      (res)=>{
-        if(res){
-          let idxToDelete = this.subjects.findIndex(s => s.id === id);
-          this.subjects.splice(idxToDelete, 1);
+  // deleteSubject(id: number){
+  //   this.subjectService.deleteSubject(id).subscribe(
+  //     (res)=>{
+  //       if(res){
+  //         let idxToDelete = this.subjects.findIndex(s => s.id === id);
+  //         this.subjects.splice(idxToDelete, 1);
 
-          this._snackBar.open(`Subject removed successfully`,'',{
-            duration: 3000
-          });
-          this.freshForm();
-        }
-      },
-      (error)=>{
-        this._snackBar.open(error,'',{
-          duration: 3000
-        });
-      }
-    );
-  }
+  //         this._snackBar.open(`Subject removed successfully`,'',{
+  //           duration: 3000
+  //         });
+  //         this.freshForm();
+  //       }
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open(error,'',{
+  //         duration: 3000
+  //       });
+  //     }
+  //   );
+  // }
 
-  toggleSubjectStatus(id: number, idx: number){
+  // toggleSubjectStatus(id: number, idx: number){
 
-    let oldState = this.subjects[idx].enabled;
-    this.subjects[idx].enabled = !oldState;
+  //   let oldState = this.subjects[idx].enabled;
+  //   this.subjects[idx].enabled = !oldState;
 
-    this.subjectService.toggleSubjectState(this.subjects[idx],id).subscribe(
-      (res) => {
-        if (res) {
-          let status = this.subjects[idx].enabled ? 'enabled' : 'disabled';
-          this._snackBar.open(`Subject ${status} successfully`, '', {
-            duration: 3000
-          });
-        }
-      },
-      (error)=>{
-        this._snackBar.open(error,'',{
-          duration: 3000
-        });
-      }
-    )
-  }
+  //   this.subjectService.toggleSubjectState(this.subjects[idx],id).subscribe(
+  //     (res) => {
+  //       if (res) {
+  //         let status = this.subjects[idx].enabled ? 'enabled' : 'disabled';
+  //         this._snackBar.open(`Subject ${status} successfully`, '', {
+  //           duration: 3000
+  //         });
+  //       }
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open(error,'',{
+  //         duration: 3000
+  //       });
+  //     }
+  //   )
+  // }
 
-  submitForm(subjectForm: any) {
+  // submitForm(subjectForm: any) {
     
-    if(!this.newSubjectData.title){
-      this._snackBar.open('Please provide subject title','',{
-        duration: 3000
-      });
-      return;
-    }
+  //   if(!this.newSubjectData.title){
+  //     this._snackBar.open('Please provide subject title','',{
+  //       duration: 3000
+  //     });
+  //     return;
+  //   }
 
-    if(this.addBtnClicked){
-      this.createSubject();
-    }else{
-      this.updateSubject();
-    }
+  //   if(this.addBtnClicked){
+  //     this.createSubject();
+  //   }else{
+  //     this.updateSubject();
+  //   }
     
-  }
+  // }
 
-  updateSubject(){
+  // updateSubject(){
 
-    this.subjectService.updateSubject(this.newSubjectData, this.newSubjectData.id).subscribe(
-      (data)=>{
-        if(data) {
-          let idxToUpdate = this.subjects.findIndex(g=>g.id === this.newSubjectData.id);
-          this.subjects[idxToUpdate].title = this.newSubjectData.title;
-          this.subjects[idxToUpdate].description = this.newSubjectData.description;
-          this.subjects[idxToUpdate].genreId = this.newSubjectData.genreId;
-          this.subjects[idxToUpdate].enabled = this.newSubjectData.enabled;
+  //   this.subjectService.updateSubject(this.newSubjectData, this.newSubjectData.id).subscribe(
+  //     (data)=>{
+  //       if(data) {
+  //         let idxToUpdate = this.subjects.findIndex(g=>g.id === this.newSubjectData.id);
+  //         this.subjects[idxToUpdate].title = this.newSubjectData.title;
+  //         this.subjects[idxToUpdate].description = this.newSubjectData.description;
+  //         this.subjects[idxToUpdate].genreId = this.newSubjectData.genreId;
+  //         this.subjects[idxToUpdate].enabled = this.newSubjectData.enabled;
           
-          this._snackBar.open('Subject updated successfully','',{
-            duration: 3000
-          });
+  //         this._snackBar.open('Subject updated successfully','',{
+  //           duration: 3000
+  //         });
   
-          this.freshForm();
-        }
+  //         this.freshForm();
+  //       }
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open(error,'',{
+  //         duration: 3000
+  //       });
+  //     }
+  //   );
+
+  // }
+
+  // createSubject(){
+  //   this.subjectService.addSubject(this.newSubjectData).subscribe(
+  //     (data)=>{
+  //       this.subjects.push(data);
+  //       this._snackBar.open(`${this.newSubjectData.title} is added successfully to list`,'',{
+  //         duration: 3000
+  //       });
+
+  //       this.freshForm();
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open(error,'',{
+  //         duration: 3000
+  //       });
+  //     }
+  //   )
+  // }
+
+  // getGenreName(genreId: number) {
+  //   return this.genresList.find(g => g.id === genreId).label;
+  // }
+
+  dataSource: MatTableDataSource<any>;
+
+  subjects: Subject[]=[];
+
+  columns: string[] = ['id','title', 'description', 'genreId','enabled']
+
+  @ViewChild(MatSort, { static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true}) paginator: MatPaginator;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.subjects = [
+      {
+        id: 1,
+        title: 'Mathematics',
+        description: 'Dummy Text',
+        genreId: 3,
+        enabled: true
       },
-      (error)=>{
-        this._snackBar.open(error,'',{
-          duration: 3000
-        });
-      }
-    );
-
-  }
-
-  createSubject(){
-    this.subjectService.addSubject(this.newSubjectData).subscribe(
-      (data)=>{
-        this.subjects.push(data);
-        this._snackBar.open(`${this.newSubjectData.title} is added successfully to list`,'',{
-          duration: 3000
-        });
-
-        this.freshForm();
+      {
+        id: 2,
+        title: 'History',
+        description: 'Dummy Text',
+        genreId: 1,
+        enabled: true
       },
-      (error)=>{
-        this._snackBar.open(error,'',{
-          duration: 3000
-        });
+      {
+        id: 3,
+        title: 'Dance',
+        description: 'Dummy Text',
+        genreId: 2,
+        enabled: true
       }
-    )
+    ]
+   
+    this.dataSource = new MatTableDataSource(this.subjects);
+
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
-  getGenreName(genreId: number) {
-    return this.genresList.find(g => g.id === genreId).label;
+  applyFilter(event: any){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
+
 
 }
