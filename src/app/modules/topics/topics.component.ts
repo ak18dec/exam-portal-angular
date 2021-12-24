cd1d929c431d28a69011708c29a0e91ee141618f
Ankit Kumar <ankitdayal1992@gmail.com>
2021-12-25 01:23:09 +0530

    Categories and Topics Updated v2

--- src/app/modules/topics/topics.component.ts
+++ src/app/modules/topics/topics.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Topic } from 'src/app/models/topic';
 import { CategoryService } from 'src/app/services/category.service';
 import { TopicService } from 'src/app/services/topic.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-topics',
@@ -11,191 +15,238 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class TopicsComponent implements OnInit {
 
-  newTopic: boolean = false;
+  // newTopic: boolean = false;
 
-  addBtnClicked: boolean = false;
-  topics: Topic[] = [];
-  categoriesList: any[] = [];
+  // addBtnClicked: boolean = false;
+  // topics: Topic[] = [];
+  // categoriesList: any[] = [];
 
-  newTopicData: any = {
-    id: 0,
-    title: '',
-    description: '',
-    categoryId: 0,
-    enabled: true
-  }
+  // newTopicData: any = {
+  //   id: 0,
+  //   title: '',
+  //   description: '',
+  //   categoryId: 0,
+  //   enabled: true
+  // }
 
-  constructor(private _snackBar: MatSnackBar, private topicService: TopicService, private categoryService: CategoryService) { }
+  // constructor(private _snackBar: MatSnackBar, private topicService: TopicService, private categoryService: CategoryService) { }
 
-  ngOnInit() {
-    this.freshForm();
-    this.getAllTopics();
-    this.categoriesList.length = 0;
-    this.categoryService.getCategories().subscribe(
-      (data: any) => {
-        data.forEach((d: { id: any; title: any; enabled: any; }) => {
-          if(d.enabled){
-            this.categoriesList.push({id: d.id, label: d.title, enabled: d.enabled});
-          }
-        });
-      },
-      (error)=>{
-        this._snackBar.open('Error while fetching categories list','',{
-          duration: 3000
-        });
-      }
-    );
-  }
+  // ngOnInit() {
+  //   this.freshForm();
+  //   this.getAllTopics();
+  //   this.categoriesList.length = 0;
+  //   this.categoryService.getCategories().subscribe(
+  //     (data: any) => {
+  //       data.forEach((d: { id: any; title: any; enabled: any; }) => {
+  //         if(d.enabled){
+  //           this.categoriesList.push({id: d.id, label: d.title, enabled: d.enabled});
+  //         }
+  //       });
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open('Error while fetching categories list','',{
+  //         duration: 3000
+  //       });
+  //     }
+  //   );
+  // }
 
-  getAllTopics() {
-    this.topicService.getTopics().subscribe(
-      (res: any) => {
-        this.topics = res;
-      },
-      (error) => {
-        this._snackBar.open('Error while fetching topics list','',{
-          duration: 3000
-        });
-      } 
-    )
-  }
+  // getAllTopics() {
+  //   this.topicService.getTopics().subscribe(
+  //     (res: any) => {
+  //       this.topics = res;
+  //     },
+  //     (error) => {
+  //       this._snackBar.open('Error while fetching topics list','',{
+  //         duration: 3000
+  //       });
+  //     } 
+  //   )
+  // }
 
-  freshForm(){
-    this.newTopic = false;
-    this.addBtnClicked = false;
-    this.newTopicData.title = '';
-    this.newTopicData.id = 0
-    this.newTopicData.description = '';
-    this.newTopicData.categoryId = 0;
-    this.newTopicData.enabled = true;
-  }
+  // freshForm(){
+  //   this.newTopic = false;
+  //   this.addBtnClicked = false;
+  //   this.newTopicData.title = '';
+  //   this.newTopicData.id = 0
+  //   this.newTopicData.description = '';
+  //   this.newTopicData.categoryId = 0;
+  //   this.newTopicData.enabled = true;
+  // }
 
-  backToTopics() {
-    this.freshForm();
-  }
+  // backToTopics() {
+  //   this.freshForm();
+  // }
 
-  addTopic() {
-    this.freshForm();
-    this.newTopic = true;
-    this.addBtnClicked = true;
-  }
+  // addTopic() {
+  //   this.freshForm();
+  //   this.newTopic = true;
+  //   this.addBtnClicked = true;
+  // }
 
-  editTopic(editableTopic: any){
-    this.newTopic = true;
-    this.addBtnClicked = false;
-    this.newTopicData.title = editableTopic.title;
-    this.newTopicData.description = editableTopic.description;
-    this.newTopicData.id = editableTopic.id;
-    this.newTopicData.enabled = editableTopic.enabled;
-    this.newTopicData.categoryId = editableTopic.categoryId;
-  }
+  // editTopic(editableTopic: any){
+  //   this.newTopic = true;
+  //   this.addBtnClicked = false;
+  //   this.newTopicData.title = editableTopic.title;
+  //   this.newTopicData.description = editableTopic.description;
+  //   this.newTopicData.id = editableTopic.id;
+  //   this.newTopicData.enabled = editableTopic.enabled;
+  //   this.newTopicData.categoryId = editableTopic.categoryId;
+  // }
 
-  deleteTopic(id: number){
-    this.topicService.deleteTopic(id).subscribe(
-      (res)=>{
-        if(res){
-          let idxToDelete = this.topics.findIndex(t => t.id === id);
-          this.topics.splice(idxToDelete, 1);
+  // deleteTopic(id: number){
+  //   this.topicService.deleteTopic(id).subscribe(
+  //     (res)=>{
+  //       if(res){
+  //         let idxToDelete = this.topics.findIndex(t => t.id === id);
+  //         this.topics.splice(idxToDelete, 1);
 
-          this._snackBar.open(`Topic removed successfully`,'',{
-            duration: 3000
-          });
-          this.freshForm();
-        }
-      },
-      (error)=>{
-        this._snackBar.open(error,'',{
-          duration: 3000
-        });
-      }
-    );
-  }
+  //         this._snackBar.open(`Topic removed successfully`,'',{
+  //           duration: 3000
+  //         });
+  //         this.freshForm();
+  //       }
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open(error,'',{
+  //         duration: 3000
+  //       });
+  //     }
+  //   );
+  // }
 
-  toggleTopicStatus(id: number, idx: number){
-    let oldState = this.topics[idx].enabled;
-    this.topics[idx].enabled = !oldState;
+  // toggleTopicStatus(id: number, idx: number){
+  //   let oldState = this.topics[idx].enabled;
+  //   this.topics[idx].enabled = !oldState;
 
-    this.topicService.toggleTopicState(this.topics[idx],id).subscribe(
-      (res) => {
-        if (res) {
-          let status = this.topics[idx].enabled ? 'enabled' : 'disabled';
-          this._snackBar.open(`Topic ${status} successfully`, '', {
-            duration: 3000
-          });
-        }
-      },
-      (error)=>{
-        this._snackBar.open(error,'',{
-          duration: 3000
-        });
-      }
-    )
-  }
+  //   this.topicService.toggleTopicState(this.topics[idx],id).subscribe(
+  //     (res) => {
+  //       if (res) {
+  //         let status = this.topics[idx].enabled ? 'enabled' : 'disabled';
+  //         this._snackBar.open(`Topic ${status} successfully`, '', {
+  //           duration: 3000
+  //         });
+  //       }
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open(error,'',{
+  //         duration: 3000
+  //       });
+  //     }
+  //   )
+  // }
 
-  submitForm(topicForm: any) {
-    if(!this.newTopicData.title){
-      this._snackBar.open('Please provide Topic title','',{
-        duration: 3000
-      });
-      return;
-    }
+  // submitForm(topicForm: any) {
+  //   if(!this.newTopicData.title){
+  //     this._snackBar.open('Please provide Topic title','',{
+  //       duration: 3000
+  //     });
+  //     return;
+  //   }
 
-    if(this.addBtnClicked){
-      this.createTopic();
-    }else{
-      this.updateTopic();
-    }
+  //   if(this.addBtnClicked){
+  //     this.createTopic();
+  //   }else{
+  //     this.updateTopic();
+  //   }
     
-  }
+  // }
 
-  updateTopic(){
+  // updateTopic(){
 
-    this.topicService.updateTopic(this.newTopicData, this.newTopicData.id).subscribe(
-      (data)=>{
-        if(data) {
-          let idxToUpdate = this.topics.findIndex(c=>c.id === this.newTopicData.id);
-          this.topics[idxToUpdate].title = this.newTopicData.title;
-          this.topics[idxToUpdate].description = this.newTopicData.description;
-          this.topics[idxToUpdate].categoryId = this.newTopicData.categoryId;
-          this.topics[idxToUpdate].enabled = this.newTopicData.enabled;
+  //   this.topicService.updateTopic(this.newTopicData, this.newTopicData.id).subscribe(
+  //     (data)=>{
+  //       if(data) {
+  //         let idxToUpdate = this.topics.findIndex(c=>c.id === this.newTopicData.id);
+  //         this.topics[idxToUpdate].title = this.newTopicData.title;
+  //         this.topics[idxToUpdate].description = this.newTopicData.description;
+  //         this.topics[idxToUpdate].categoryId = this.newTopicData.categoryId;
+  //         this.topics[idxToUpdate].enabled = this.newTopicData.enabled;
           
-          this._snackBar.open('Topic updated successfully','',{
-            duration: 3000
-          });
+  //         this._snackBar.open('Topic updated successfully','',{
+  //           duration: 3000
+  //         });
   
-          this.freshForm();
-        }
-      },
-      (error)=>{
-        this._snackBar.open(error,'',{
-          duration: 3000
-        });
-      }
-    );
+  //         this.freshForm();
+  //       }
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open(error,'',{
+  //         duration: 3000
+  //       });
+  //     }
+  //   );
 
+  // }
+
+  // createTopic(){
+  //   this.topicService.addTopic(this.newTopicData).subscribe(
+  //     (data)=>{
+  //       this.topics.push(data);
+  //       this._snackBar.open(`${this.newTopicData.title} is added successfully to list`,'',{
+  //         duration: 3000
+  //       });
+
+  //       this.freshForm();
+  //     },
+  //     (error)=>{
+  //       this._snackBar.open(error,'',{
+  //         duration: 3000
+  //       });
+  //     }
+  //   )
+  // }
+
+  // getCategoryName(categoryId: number) {
+  //   return this.categoriesList.find(c => c.id === categoryId).label;
+  // }
+
+  dataSource: MatTableDataSource<any>;
+
+  topics: Topic[]=[];
+
+  columns: string[] = ['id','title', 'description', 'categoryId','enabled']
+
+  @ViewChild(MatSort, { static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true}) paginator: MatPaginator;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.topics = [
+      // {
+      //   id: 1,
+      //   title: 'Mathematics',
+      //   description: 'Dummy Text',
+      //   genreId: 3,
+      //   enabled: true
+      // },
+      // {
+      //   id: 2,
+      //   title: 'History',
+      //   description: 'Dummy Text',
+      //   genreId: 1,
+      //   enabled: true
+      // },
+      // {
+      //   id: 3,
+      //   title: 'Dance',
+      //   description: 'Dummy Text',
+      //   genreId: 2,
+      //   enabled: true
+      // }
+    ]
+   
+    this.dataSource = new MatTableDataSource(this.topics);
+
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
-  createTopic(){
-    this.topicService.addTopic(this.newTopicData).subscribe(
-      (data)=>{
-        this.topics.push(data);
-        this._snackBar.open(`${this.newTopicData.title} is added successfully to list`,'',{
-          duration: 3000
-        });
-
-        this.freshForm();
-      },
-      (error)=>{
-        this._snackBar.open(error,'',{
-          duration: 3000
-        });
-      }
-    )
+  applyFilter(event: any){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
-  getCategoryName(categoryId: number) {
-    return this.categoriesList.find(c => c.id === categoryId).label;
-  }
-
 
 }
