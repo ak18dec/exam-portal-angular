88dd064ca3921e7939597153ce16c2bb8bb786cb
ak18dec <ankitdayal1992@gmail.com>
2025-05-16 21:04:12 +0530

    User Portal Mat Toolbar CSS Fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -111,7 +111,7 @@ const routes: Routes = [
   {
     path: 'user',
     component: UserHomeComponent,
-    canActivate: [UserGuard],
+    // canActivate: [UserGuard],
     children: [
       {
         path: '',
