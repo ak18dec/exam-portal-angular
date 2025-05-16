b716e05537352b2378632c38a62315d4c3ea7c34
ak18dec <ankitdayal1992@gmail.com>
2025-05-17 01:04:25 +0530

    Generate Quiz Feature Added on User Quizzes Portal

--- src/app/modules/user-quiz-list/user-quiz-list.component.ts
+++ src/app/modules/user-quiz-list/user-quiz-list.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { UntypedFormControl } from "@angular/forms";
 import { debounceTime, switchMap, startWith } from "rxjs/operators";
 import { of } from "rxjs";
 import { Router } from '@angular/router';
 import { QuizService } from 'src/app/services/quiz.service';
+import { CategoryService } from 'src/app/services/category.service';
+import { Category } from 'src/app/models/category';
 @Component({
     selector: 'app-user-quiz-list',
     templateUrl: './user-quiz-list.component.html',
@@ -12,47 +14,51 @@ import { QuizService } from 'src/app/services/quiz.service';
 })
 export class UserQuizListComponent implements OnInit {
 
-  search = new UntypedFormControl();
-  data: any[] = [];
-  dataLoaded: boolean = false;
+  categoryService = inject(CategoryService)
+  categories: Category[] = []
 
-  constructor(private router: Router, private quizService: QuizService) { }
+  // search = new UntypedFormControl();
+  // data: any[] = [];
+  // dataLoaded: boolean = false;
+
+  // constructor(private router: Router, private quizService: QuizService) { }
 
   ngOnInit(): void {
     // this.getAllQuizes();
+    this.categories = this.categoryService.getCategories();
   }
 
-  filteredList$ = this.search.valueChanges.pipe(
-    startWith(null),
-    debounceTime(200),
-    switchMap((res: string) => {
-      if (!res) return of(this.data);
-      res = res.trim().toLowerCase();
-      return of(
-        this.data.filter(x => x.title.toLowerCase().indexOf(res) >= 0)
-      );
-    })
-  );
+  // filteredList$ = this.search.valueChanges.pipe(
+  //   startWith(null),
+  //   debounceTime(200),
+  //   switchMap((res: string) => {
+  //     if (!res) return of(this.data);
+  //     res = res.trim().toLowerCase();
+  //     return of(
+  //       this.data.filter(x => x.title.toLowerCase().indexOf(res) >= 0)
+  //     );
+  //   })
+  // );
 
-  startQuiz(quizId: number) {
-    this.router.navigate([`/user/quiz/${quizId}`]);
-  }
+  // startQuiz(quizId: number) {
+  //   this.router.navigate([`/user/quiz/${quizId}`]);
+  // }
 
-  getAllQuizes(){
-    this.data = this.quizService.getQuizesFromCache();
-    if(this.data.length < 1) {
-      this.quizService.getQuizes().subscribe(
-        (res: any) =>{
-          this.data = res;
-          this.quizService.storeQuizesInCache(this.data);
-          this.dataLoaded = true;
-          this.search.setValue('');
-        },
-        (error) => {
-          console.log(error)
-        }
-        )
-    }
-  }
+  // getAllQuizes(){
+  //   this.data = this.quizService.getQuizesFromCache();
+  //   if(this.data.length < 1) {
+  //     this.quizService.getQuizes().subscribe(
+  //       (res: any) =>{
+  //         this.data = res;
+  //         this.quizService.storeQuizesInCache(this.data);
+  //         this.dataLoaded = true;
+  //         this.search.setValue('');
+  //       },
+  //       (error) => {
+  //         console.log(error)
+  //       }
+  //       )
+  //   }
+  // }
 
 }
